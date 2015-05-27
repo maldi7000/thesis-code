2f9419ba28ceb5e1949a49b9a06d5d91687c0924
maldi7000 <thomas.madlener@gmail.com>
2015-05-27 21:11:41 +0200

    still getting the hang of git it seems

--- makefile
+++ makefile
@@ -1,11 +1,16 @@
 # this is a very simple makefile that is primarly only here to be able to compile directly from emacs
+# NOTE: at the moment this does not compile unless a basf2 environment is set up! (it also does not run!)
 
 CC=g++
-CXXFLAGS=-std=c++11 -Wall
-INCL=-I/home/Applications/root/include
+CXXFLAGS=-std=c++11 -Wall -g
+INCL=-I/home/asehephy/root/include
+#INCL=-I/home/Applications/root/include
+# INCL=-I$(shell root-config --incdir)
 LIB=$(shell root-config --libs) # add all root libraries. CAUTION! this gets the environment variables from the shell in which emacs was started!
 
-default: root2dat
+
+
+all: root2dat
 
 root2dat: samples_root2dat.cc
 	$(CC) $(LIB) $(INCL) $(CXXFLAGS) samples_root2dat.cc
--- samples_root2dat.cc
+++ samples_root2dat.cc
@@ -20,24 +20,30 @@
 
 
 using namespace std;
+using namespace ROOT;
 
 /** name of the TTree in the root file*/
 const std::string treename = "ThreeHitSamplesTree";
 /** name of the branches in the TTree */
 const std::vector<std::string> branchnames = { "hit1X", "hit1Y", "hit1Z", "hit2X", "hit2Y", "hit2Z",
-                                               "hit3X", "hit3Y", "hit3Z", "signal" };
+                                               "hit3X", "hit3Y", "hit3Z", "layer1", "layer2", "layer3", "signal" };
 
-/** number of all branches containing a vector<double> (i.e. all, but the signal branch) */
-const size_t nbranches = 9;
+/** number of all branches containing a vector<double> */
+const size_t npositions = 9;
+
+/** number of all branches containing a vector<short> */
+const size_t nlayers = 3;
 
 /** helper struct that can be used to get the values from the root file */
 struct RootBranches {
   /** empty ctor initializes all pointers to NULL */
   RootBranches() : signal(NULL) {
     for(vector<double>*& vec : positions) vec = NULL;
+    for(vector<int>*& vec: layers) vec = NULL;
   }
-  
-  std::array<std::vector<double>*, nbranches> positions; /**< array of vector of doubles that hold the position branches*/
+
+  std::array<std::vector<double>*, npositions> positions; /**< array of vector of doubles that hold the position branches */
+  std::array<std::vector<int>*, nlayers> layers; /**< array of vector of doubles that hold the layer branches */
   std::vector<bool>* signal; /**< vector of bools for the signal branch */
 };
 
@@ -46,12 +52,17 @@ struct RootBranches {
  * @param: tree, the TTree containing the data
  * @param: branches, the RootBranches helper struct that will be set as the addresses of the branches
  */
-   
 void setBranchAddresses(TTree* tree, RootBranches& branches)
 {
-  for(size_t i = 0; i < nbranches; ++i) {
+  for(size_t i = 0; i < npositions; ++i) {
     if(tree->SetBranchAddress(branchnames[i].c_str(), &branches.positions[i]) != 0) {
-      cout << "ERROR: while trying to set branch address for positions" << endl;
+      cout << "ERROR: while trying to set branch address for positions! name: " << branchnames[i] << endl;
+    }
+  }
+  for(size_t i = 0; i < nlayers; ++i) {
+    int setAd = tree->SetBranchAddress(branchnames[i + npositions].c_str(), &branches.layers[i]);
+    if( setAd != 0 /*&& setAd != 1*/) {
+      cout << "ERROR: while trying to set branch address for layers! name: " << branchnames[i + npositions] << endl;
     }
   }
 
@@ -64,11 +75,38 @@ void setBranchAddresses(TTree* tree, RootBranches& branches)
  * get the data from the event onto the RootBranches, from where they can than be written to the file
  * @param: tree, the TTree containing the data
  * @param: branches, the RootBranches helper struct to which the data will be transfered
- * @param: event, the event number for which the data shall be obtained 
+ * @param: event, the event number for which the data shall be obtained
  */
 void getEvent(TTree* tree, RootBranches& branches, unsigned event)
 {
   tree->GetEvent(event);
+}
+
+/**
+ * write a file header that hold some additional information
+ * @param: outifle, outfile stream to which the header is written
+ */
+void writeFileHeader(ofstream& outfile)
+{
+  outfile << "# sp_1_x  sp_1_y  sp_1_z  sp_2_x  sp_2_y  sp_2_z  sp_3_x  sp_3_y  sp_3_z  layer1  layer2  layer3  signal" << endl;
+}
+
+/**
+ * write the content of the RootBranches helper struct to the file
+ * @param: branches, the RootBranches helper struct, which contents shall be written to a .dat file
+ * @param: outfile, the outfile streamer to which the data shall be written
+ */
+void writeToFile(const RootBranches& branches, ofstream& outfile)
+{
+  for(size_t i = 0; i < branches.signal->size(); ++i) {
+    for (size_t j = 0; j < npositions; ++j){
+      outfile << branches.positions[j]->operator[](i) << " ";
+    }
+    for (size_t j = 0; j < nlayers; ++j ) {
+      outfile << branches.layers[j]->operator[](i) << " ";
+    }
+    outfile << branches.signal->operator[](i) << endl;
+  }
 }
 
 /**
@@ -85,6 +123,15 @@ void convertToDatFile(char* filename, char* outfilename)
   setBranchAddresses(tree, branches);
 
   ofstream outfile(outfilename, ofstream::out);
+  // writeFileHeader(outfile); // ommit when using with MATLAB (TODO: find an easy (and fast) way in MATLAB to ignore comments)
+  
+  // get the data event-wise and write them to the file immediately
+  for(unsigned i = 0; i < tree->GetEntries(); ++i) {
+    getEvent(tree, branches, i);
+    writeToFile(branches, outfile);
+  }
+
+  outfile.close();
 }
 
 #ifndef __CINT__
@@ -94,8 +141,12 @@ void convertToDatFile(char* filename, char* outfilename)
  */
 int main(int argc, char* argv[])
 {
+  if(argc != 3) {
+    cout << "please provide a root file and an output file name!" << endl;
+    return -1;
+  }
   convertToDatFile(argv[1], argv[2]);
-  
+
   return 0;
 }
 #endif
