dbba1c059632353ce30e172480a600ed946e584a
maldi7000 <thomas.madlener@gmail.com>
2015-06-09 22:50:13 +0200

    created decorrelate function

---
+++ MATLAB/decorrelate.m
@@ -0,0 +1,42 @@
+function [Z] = decorrelate(X, varargin)
+%DECORRELATE perform decorrelation on given data
+%
+% Z = decorrelate(X, normalize) takes NxM data matrix X and returns decorrelated NxM 
+% data matrix Z, where N is the number of observations and M is the
+% dimension of one variable. If normalize is true (default is false) the
+% returned data will be normalized such that the covariance matrix is the
+% unity matrix.
+%
+% TODO: finish documentation
+% TODO: implement
+
+% by Thomas Madlener, 2015
+
+if ~isempty(varargin)
+    validateattributes(varargin{1}, {'logical'}, {'nonnan'}, 'decorrelate', 'normalize');
+    normalize = varargin{1};
+else
+    normalize = false;
+end
+
+% calculate covariance matrix estimate
+% [n, m] = size(X);
+% ybar = 1/n*sum(X); % calculate the mean vector
+% C = 1/n * (X')* X - ybar'*ybar;
+
+% using MATLAB built-in to calculate the empirical covariance matrix
+% NOTE: this is the same calculation as above! (only prefactor 1/n is
+% 1/(n-1) for cov
+C = cov(X);
+
+% do eigen decomposition of covariance matrix
+[U,D] = eig(C);
+
+Z = X * U;
+% normalized output (i.e. cov(Z) = identity)
+if normalize
+    Z = Z * 1/sqrt(D)';
+end
+
+end
+
