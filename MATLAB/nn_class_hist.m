fe2c345586b0c098a2587ebec14db7057420fc22
maldi7000 <thomas.madlener@gmail.com>
2015-06-10 17:47:03 +0200

    first implementation, still TODO: documentation and input check

--- MATLAB/nn_class_hist.m
+++ MATLAB/nn_class_hist.m
@@ -8,7 +8,23 @@ function [ out ] = nn_class_hist(t,y)
 
 % by Thomas Madlener, 2015
 
+mins = min(y,[],2); % get min and max values for every network output
+maxs = max(y,[],2);
 
+nP = ceil(sqrt(size(y,1)));
 
+pad = figure;
+for i=1:length(mins)
+    subplot(nP,nP,i);
+    A = linspace(mins(i), maxs(i), 100);
+    hall = histogram(y(i,:),A);
+    hold on
+    htrue = histogram(y(i,t==1),A);
+    hold off
+    title(sprintf('net %d', i));
+    xlabel('network output');
+    ylabel('entries');
+    legend('all samples', 'signal samples', 'Location', 'Best');
+end
 end
 
