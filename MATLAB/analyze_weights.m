8503a113306a86cb41f8112a3e05129877290210
maldi7000 <thomas.madlener@gmail.com>
2015-06-13 21:47:55 +0200

    commented out lines causing issues on R2013b, beginning of redesign and implementation of desired functionality

--- MATLAB/analyze_weights.m
+++ MATLAB/analyze_weights.m
@@ -43,11 +43,12 @@ W_ho = net.LW{2,1}; % get weight matrix of hidden layer - output layer
 % figure;
 % hist(W_ho, 50);
 
-draw_net(W_ih, W_ho);
+net_in = []; % TODO: fill this if something is wanted
+draw_net(W_ih, W_ho, net_in);
 end
 
 %% plotting functions
-function handle = draw_net(H,O) % H -> input - hidden, O -> hidden - output
+function handle = draw_net(H,O, IN) % H -> input - hidden, O -> hidden - output, IN -> input
     handle = figure; hold on, % axis equal
     axis off
     set(gcf, 'color', [1,1,1]); % white background
@@ -56,7 +57,6 @@ function handle = draw_net(H,O) % H -> input - hidden, O -> hidden - output
     maxsize = max(sizes); maxind = find(sizes == maxsize);
 
     [inpos_x, hidpos_x, outpos_x] = get_x_positions();
-    radius = .8;
     [inpos_y, hidpos_y, outpos_y] = get_y_positions(sizes);
 
     max_w = max([max(H), max(O)]); % determine max and min weight
@@ -67,10 +67,25 @@ function handle = draw_net(H,O) % H -> input - hidden, O -> hidden - output
 
     % draw lines first -> circles are above lines then later
     handle = draw_weight_lines(handle, H, i_h_cols, O, h_o_cols);
+  
+    [hid_v, out_v] = calc_neuron_vals(H,O,IN,'input'); % TODO: need activation function of each layer here!
+    
+    v_min = min([hid_v, out_v]); % TODO: also include input values here!
+    v_max = max([hid_v, out_v]);
+    
+    incolors = get_circle_colors(IN, v_min, v_max);
+    hidcolors = get_circle_colors(IN, v_min, v_max);
+    outolors = get_circle_colors(IN, v_min, v_max);
+    
+    % draw neurons
+    handle = draw_neuron_circles(handle, inpos_x, inpos_y, incolors);
+    handle = draw_neuron_circles(handle, hidpos_x, hidpos_y, incolors);
+    handle = draw_neuron_circles(handle, outpos_x, outpos_y, incolors);
+    
     % for now do not color the neurons (might come later)
-    circles(inpos_x,inpos_y, radius, 'facecolor', 'none');
-    circles(hidpos_x,hidpos_y, radius, 'facecolor', 'none');
-    circles(outpos_x,outpos_y, radius, 'facecolor', 'none');
+%     circles(inpos_x,inpos_y, radius, 'facecolor', 'none');
+%     circles(hidpos_x,hidpos_y, radius, 'facecolor', 'none');
+%     circles(outpos_x,outpos_y, radius, 'facecolor', 'none');
 
     % plot additional info
     text(inpos_x - 3, inpos_y(1) - 3, 'Input');
@@ -84,7 +99,7 @@ end
 % h is the figure handle
 % H_cols and O_cols define the colors of the lines
 function handle = draw_weight_lines(h, H, H_cols, O, O_cols)
-    set(groot, 'CurrentFigure', h);
+%     set(groot, 'CurrentFigure', h); % does not work on R2013b, why?
     [y_in, y_hid, y_out] = get_y_positions([size(H'), size(O)]);
     [x_in, x_hid, x_out] = get_x_positions();
 
@@ -105,13 +120,22 @@ function handle = draw_weight_lines(h, H, H_cols, O, O_cols)
     handle = h; % does this work?
 end
 
+% draw neuron circles
+function handle = draw_neuron_circles(h, x, y, colors)
+    %     set(groot, 'CurrentFigure', h); % does not work on R2013b, why?
+    radius = .8;
+    circles(x,y,radius,'facecolor', 'none');
+    % TODO: coloring
+    handle = h;
+end
+
 % draw legend for understanding the meaning of the colors
 function handle = draw_legend(h, w_min, w_max)
-    set(groot, 'CurrentFigure', h);
+%     set(groot, 'CurrentFigure', h); % does not work on R2013b, why?
     colmap = colormap(get_colormap(51));
     caxis([w_min, w_max]);
     c = colorbar;
-    c.Label.String = 'Absolute value of weight';
+    c.Label.String = 'Absolute value of weight'; % doesnot work in R2013b
 
     handle = h; % works?
 end
@@ -155,6 +179,21 @@ function linecols = get_line_colors(M, w_max, w_min)
             linecols{i,j} = colmap(col(end),:); % TODO
         end
     end
+end
+
+% calculate the values the neurons have. options: input -> input value,
+% activation -> activation-function value corresponding to input (= neuron
+% output)
+function [HV, OV] = calc_neuron_vals(IN,H,O,option)
+    
+    HV = [];
+    OV = [];
+end
+
+% determine the colors of the neuron circles
+function circlecols = get_circle_colors(V, v_max, v_min)
+    
+    circlecols = {}; % return empty at the moment
 end
 
 % define colormap for this function
