cda78fabb0f0cebea2646a0c488449d8090d6f5c
maldi7000 <thomas.madlener@gmail.com>
2015-06-14 17:56:19 +0200

    first more or less functional version for weights and weightsabs

--- MATLAB/draw_net.m
+++ MATLAB/draw_net.m
@@ -1,9 +1,24 @@
-function [ output_args ] = draw_net(net)
+function [ output_args ] = draw_net(net,varargin)
 %DRAW_NET draws the given neural net
 %
-% draw_net(NET) takes as input a neural network (at the moment only
-% a feedforward network with one hidden layer) and analyzes its weights and
-% its relations between different neurons
+% draw_net(NET,...) takes as input a neural network (at the moment only
+% a feedforward network with one hidden layer) and draws its structure and
+% some additional information that can be controlled via OPTION.
+%
+% possible inputs for OPTION:
+% + 'plain': simply plot the structure of the network (default) (DONE)
+% + 'weigtsabs': plot the structure of the network. The color of each
+% connections depends on the absolute weight of the corresponding weight (DONE)
+% + 'weights': plot the structure of the network. The color of each
+% connections depends on the value of the corresponding weight (TODO)
+% + 'input': plot the structure of the network. The color of each
+% connection depends on the actual signal value (= weight *
+% connection-input). The color of each neuron depends on the neuron input
+% (= sum of ingoing signals). (TODO)
+% + 'activation': plot the structure of the network. The color of each
+% connection depends on the actual signal value (= weight *
+% connection-input). The color of each neuron depends on the neuron
+% activation value (= output of neuron). (TODO)
 %
 % At the moment it plots the neural network and its connections where the
 % connections are colored according to the weight of the connection.
@@ -22,26 +37,42 @@ function [ output_args ] = draw_net(net)
 % by Thomas Madlener, 2015
 
 %% input checks and handling
-if ~isa(net, 'network'), error('input needs to be a network'), end
+p = inputParser; % use input parser for handling options and validating stuff
+addRequired(p, 'net', @(x) isa(x, 'network'));
+
+expectedOptions = {'plain', 'weightsabs', 'weights', 'input', 'activation'};
+defaultOption = 'plain';
+addOptional(p,'option',defaultOption, ...
+    @(x) any(validatestring(x,expectedOptions)));
+
+parse(p,net,varargin{:});
+option = p.Results.option;
+
 if length(net.layers) > 2, error('can only handle networks with one hidden layer at the moment'), end
-% TODO: more checks?
 
 %% main function body
 W_ih = net.IW{1}; % get weigh matrix of input layer - hidden layer
 W_ho = net.LW{2,1}; % get weight matrix of hidden layer - output layer
-% [hidsize, insize] = size(W_ih);
-% outsize = size(W_ho,1);
-%
-% hidkeys = cell(insize,1);
-% for i=1:insize, hidkeys{i} = sprintf('input %d', i); end
-% W_ih_vec = reshape(W_ih,[],1); % reshape to (column) vector
-% hist(W_ih, 50); % make histogram split by inputs
-% hold on
-% hist(W_ih_vec,50);
-% legend(hidkeys, 'Location', 'Best');
 
-% figure;
-% hist(W_ho, 50);
+% set circle values to zeros by default
+inV = zeros(size(W_ih,2),1);
+hidV = zeros(size(W_ih,1),1);
+oV = zeros(size(W_ho,1),1);
+
+if strcmp(option, 'plain') % if plain, simply give all weights the same weight
+    W_ih = zeros(size(W_ih));
+    W_ho = zeros(size(W_ho));
+end
+if strcmp(option, 'weightsabs')
+    W_ih = abs(W_ih);
+    W_ho = abs(W_ho);
+end
+if strcmp(option, 'input')
+   % TODO: change neuron values
+end
+if strcmp(option, 'activation')
+    % TODO: change neuron values
+end
 
 % new version
 max_w = max([max(W_ih), max(W_ho)]);
@@ -54,11 +85,12 @@ inc = cell(size(W_ih,2),1);
 hidc = cell(size(W_ih,1),1);
 outc = cell(size(W_ho,1),1);
 
-inc = get_circle_colors(inc, 0, 1);
-hidc = get_circle_colors(hidc, 0, 1);
-outc = get_circle_colors(outc, 0, 1);
+inc = get_circle_colors(inV, max(inV), min(inV));
+hidc = get_circle_colors(hidV, max(hidV), min(hidV));
+outc = get_circle_colors(oV, max(oV), min(oV));
 
 create_drawing(inc,hidc,outc,in_h_c,h_o_c);
+draw_legend(max_w, min_w); % TODO: fix, needs handle?
 end
 
 %% plotting functions
@@ -71,14 +103,12 @@ function handle = create_drawing(I, H, O, ih, ho)
     
     handle = draw_weight_lines(handle, ih, ho);
     
-    [ip_x, hp_x, op_x] = get_x_positions();
+    [ip_x, hp_x, op_x] = get_x_positions(); % get neuron x- & y-positions
     [ip_y, hp_y, op_y] = get_y_positions([size(ih'), size(ho,1)]);
     
     handle = draw_neuron_circles(handle, ip_x, ip_y, I);
     handle = draw_neuron_circles(handle, hp_x, hp_y, H);
     handle = draw_neuron_circles(handle, op_x, op_y, O);
-    
-    % TODO: rewrite legend-stuff
 end
 
 % new version of draw_weight_lines
@@ -116,14 +146,14 @@ function handle = draw_neuron_circles(h, x, y, colors)
 end
 
 % draw legend for understanding the meaning of the colors
-function handle = draw_legend(h, w_min, w_max)
+function handle = draw_legend(w_max, w_min)
 %     set(groot, 'CurrentFigure', h); % does not work on R2013b, why?
-    colmap = colormap(get_colormap(51));
+    colmap = colormap(get_colormap(w_max, w_min));
     caxis([w_min, w_max]);
     c = colorbar;
     c.Label.String = 'Absolute value of weight'; % doesnot work in R2013b
 
-    handle = h; % works?
+%     handle = h; % works?
 end
 
 %% helper stuff
@@ -150,16 +180,14 @@ end
 
 % get the linecolors for every line (returns a NxM cell-array with a 1x3 rgb color
 % for every matrix entry
-% NOTE: at the moment only greyscale of absvalue of weight
 function linecols = get_line_colors(M, w_max, w_min)
-    abs_max = max([abs(w_max), abs(w_min)]);
     linecols = cell(size(M)); % preallocate
-    colmap = get_colormap(51); % WARNING: the value of 51 is also used in other places!
-    col_int = linspace(0,abs_max,51); % define an intervall for each color
+    colmap = get_colormap(w_max, w_min); % WARNING: the value of 51 is also used in other places!
+    col_int = linspace(w_min, w_max, length(colmap)); % define an intervall for each color
 %     col_int = linspace(log(1e-1),log(abs_max),51); % define logarithmic scale
     for i=1:size(M,1)
         for j=1:size(M,2)
-            col = find(col_int <= abs(M(i,j)));
+            col = find(col_int <= M(i,j));
 %             col = find(col_int <= log(abs(M(i,j))));
 %             if isempty(col), col = 1; end % safety measure
             linecols{i,j} = colmap(col(end),:); % TODO
@@ -179,13 +207,34 @@ end
 % determine the colors of the neuron circles
 function circlecols = get_circle_colors(V, v_max, v_min)
     circlecols = cell(size(V));
-    circlecols(:) = {'none'}; % assign no color to cells, for now
+    if v_max == v_min % temporary solution for non colored neurons
+        circlecols(:) = {'none'}; % assign no color to cells, for now
+    end
 %     circlecols = {}; % return empty at the moment
 end
 
 % define colormap for this function
 % COULDDO: make this take options (e.g. greyscale, etc.)
-function colmap = get_colormap(steps)
-    x = linspace(1,0,steps); % for greyscale
-    colmap = [x', x', x']; % greyscal color map
+% desired: 
+function colmap = get_colormap(v_max, v_min)
+    steps = 50;
+    if v_min == 0
+       ng = round(steps / 3);
+       nr = 0;
+    else
+        r = abs(v_max/v_min);
+        ng = round(steps  * r / 3);
+        nr = round(steps /r / 3);
+    end
+    gm = [ones(ng,1); linspace(1,0.5,2*ng)'];
+    gh = [linspace(1,0,2*ng)'; zeros(ng,1)];
+    rm = [linspace(0.5,1,2*nr)'; ones(nr,1)];
+    rh = [zeros(nr,1); linspace(0,1,2*nr)'];
+    
+    colmap = colormap([rm, rh, rh;gh,gm,gh]); % avoid entry 1,1,1 twice
+
+%     m = [ones(n,1); linspace(1,0.5,2*n)'];
+% %     h = [zeros(2*n,1); linspace(0,1,n)'];
+%     h = [linspace(1,0,2*n)'; zeros(n,1)];
+%     colmap = [h,m,h]; % greyscal color map
 end
