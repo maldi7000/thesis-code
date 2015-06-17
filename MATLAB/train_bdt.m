3c4d311d302f16c166da7dc05338a6dc585ad8ae
maldi7000 <thomas.madlener@gmail.com>
2015-06-17 13:53:48 +0200

    first version

---
+++ MATLAB/train_bdt.m
@@ -0,0 +1,22 @@
+function [ bdt ] = train_bdt(X,T,N, varargin)
+%TRAIN_BDT trains a boosted decision tree
+%
+% BDT = TRAIN_BDT(X,T,N,Name,Value) trains a boosted decision tree
+% (fitensemble) on the input data X and the target classes T. 
+% N is the number of boosts (trees) that are used in the training. The
+% Name,Value pairs can be used to set some properties of the trees that
+% should be used for the training
+%
+% See also fitensemble, templateTree
+
+% by Thomas Madlener, 2015
+
+%% input checks necessary?
+
+%% define a treeTemplate that is used for boosting and also the boosting algorithm
+tempTree = templateTree(varargin{:}); % pass along varargin to tempTree
+boostAlg = 'AdaBoostM1'; % default is AdaBoostM1
+
+bdt = fitensemble(X,T,boostAlg,N,tempTree,...
+    'nprint',25);
+end
