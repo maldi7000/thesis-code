6eb12ec00386d5d3596cf7e5ad88c0dd271c2252
maldi7000 <thomas.madlener@gmail.com>
2015-06-17 15:45:14 +0200

    added learning Rate as parameter

--- MATLAB/train_bdt.m
+++ MATLAB/train_bdt.m
@@ -1,9 +1,10 @@
-function [ bdt ] = train_bdt(X,T,N, varargin)
+function [ bdt ] = train_bdt(X,T,N,lRate,varargin)
 %TRAIN_BDT trains a boosted decision tree
 %
-% BDT = TRAIN_BDT(X,T,N,Name,Value) trains a boosted decision tree
+% BDT = TRAIN_BDT(X,T,N,lRate,Name,Value) trains a boosted decision tree
 % (fitensemble) on the input data X and the target classes T. 
-% N is the number of boosts (trees) that are used in the training. The
+% N is the number of boosts (trees) that are used in the training. lRate 
+% is the learning rate (AdaBoost default is 1). The
 % Name,Value pairs can be used to set some properties of the trees that
 % should be used for the training
 %
@@ -18,5 +19,5 @@ tempTree = templateTree(varargin{:}); % pass along varargin to tempTree
 boostAlg = 'AdaBoostM1'; % default is AdaBoostM1
 
 bdt = fitensemble(X,T,boostAlg,N,tempTree,...
-    'nprint',25);
+    'nprint',25, 'LearnRate', lRate);
 end
