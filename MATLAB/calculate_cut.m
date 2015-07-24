25020098fd6d24fae0684df8fc4b0c6ebd4f7de5
maldi7000 <thomas.madlener@gmail.com>
2015-07-24 18:17:37 +0200

    added automatic calculation of boundaries to be passed to analyze_calc_out. COULDDO: I think the functionality can be established without the use of analyze_class_out

--- MATLAB/calculate_cut.m
+++ MATLAB/calculate_cut.m
@@ -14,11 +14,19 @@ if nargin < 3, efficiency = 0.99; end
 if length(targets) ~= length(outputs), error('targets and output must be of the same length'), end
 if ~isvector(targets) || ~isvector(outputs), error('targets and outputs have to be passed as vectors'), end
 
-lb = min(outputs);
-ub = lb + (max(outputs) - lb) / 2;
+[lb, ub] = calc_boundaries(outputs,targets, efficiency)
 [~,R,C] = analyze_class_out(targets,outputs,lb,ub);
 gtr = R >= efficiency;
 c = C(gtr); % get all possible cut values (yielding a good enough efficiency)
 cut = c(end); % choose the loosest cut
 end
 
+%% helper function
+% calculate the lower and upper boundary to be used to calculate the
+% cut value (via the analyze_class_out function)
+function [lb, ub] = calc_boundaries(y,t,r)
+    lb = min(y);
+    [c,e] = histcounts(y(t==1),100);
+    ind = cumsum(c) > (1 -r ) * length(y(t==1));
+    ub = min(e(ind));
+end
