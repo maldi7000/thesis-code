4d038828e61cb02783469b944a6bf3a41e4fee11
maldi7000 <thomas.madlener@gmail.com>
2015-07-24 16:40:37 +0200

    brought arguments in the right order

--- MATLAB/analyze_class_angular.m
+++ MATLAB/analyze_class_angular.m
@@ -27,8 +27,8 @@ cutvalue = calculate_cut(targets,outputs,0.99);
 [~,theta,phi] = cart2sph_basf2(inputs(:,1:3));
 % analyze_class_bins_dep(targets,outputs,phi,nPhiBins, '\phi [\circ]'); % old version of function for other plots
 % analyze_class_bins_dep(targets,outputs,theta,nThetaBins, '\theta [\circ]'); % old version of function for other plots
-analyze_class_bins(targets,outputs,cutvalue,phi,nPhiBins, '\phi [\circ]');
-analyze_class_bins(targets,outputs,cutvalue,theta,nThetaBins, '\theta [\circ]');
+analyze_class_bins(targets,outputs,phi,nPhiBins, cutvalue,'\phi [\circ]');
+analyze_class_bins(targets,outputs,theta,nThetaBins, cutvalue, '\theta [\circ]');
 end
 
 %% helper functions
