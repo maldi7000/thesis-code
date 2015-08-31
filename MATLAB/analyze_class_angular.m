b32b87e1513f35a5e37a5d671821b535058bb4d0
maldi7000 <thomas.madlener@gmail.com>
2015-08-31 20:02:35 +0200

    explicitly converting cartesion to spherical coordinates the same way it is done in ROOT::TVector3

--- MATLAB/analyze_class_angular.m
+++ MATLAB/analyze_class_angular.m
@@ -30,7 +30,7 @@ if nargin < 4 % calculate cutvalue if necessary
     cutvalue = calculate_cut(targets,outputs,0.99);
 end
 %% function main body
-[~,theta,phi] = cart2sph_basf2(inputs(:,1:3));
+[theta,phi] = cart2sph_basf2(inputs(:,1:3));
 % analyze_class_bins_dep(targets,outputs,phi,nPhiBins, '\phi [\circ]'); % old version of function for other plots
 % analyze_class_bins_dep(targets,outputs,theta,nThetaBins, '\theta [\circ]'); % old version of function for other plots
 if nargout > 1
@@ -48,9 +48,19 @@ end
 end
 %% helper functions
 % transform cartesian coordinates X (Nx3 matrix) to spherical coordinates
-% as used in BASF2
-function [r,theta,phi] = cart2sph_basf2(X)
-    [phi,theta,r] = cart2sph(X(:,1), X(:,2), X(:,3)); % angles in rad
-    phi = phi * 180 / pi; % phi [-180,180] -> positive x-axis is 0
-    theta = 90 - theta * 180 / pi; % theta [0,180] <-> [front, back]
+% as used in BASF2 (defined by ROOT there) -> returns degrees
+function [theta,phi] = cart2sph_basf2(X)
+% $$$     [phi,theta,r] = cart2sph(X(:,1), X(:,2), X(:,3)); % angles in rad
+% $$$     phi = phi * 180 / pi; % phi [-180,180] -> positive x-axis is 0
+% $$$     theta = 90 - theta * 180 / pi; % theta [0,180] <-> [front, back]
+    perp = sqrt(X(:,1).*X(:,1) + X(:,2).*X(:,2));
+    xyzero = any(perp == 0, 2); % check if X AND Y are zero
+    zzero = any(X(:,3) == 0, 2); % check if Z is zero
+    phi = atan2(X(:,2), X(:,1)) .* (~xyzero) * 180/pi; % if X and Y are zero,
+                                             % return 0 (done by
+                                             % negating)
+    theta = atan2(perp, X(:,3)) .* (~xyzero) .* (~zzero) * 180/pi; % if X, Y
+                                                        % and Z are
+                                                        % zero
+                                                        % return 0
 end
